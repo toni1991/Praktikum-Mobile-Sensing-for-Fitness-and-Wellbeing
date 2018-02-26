025ec4ab40032cdb59234d08047fced3b1a99524
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-02-26 14:27:07 +0100

    Changed sensor class.

--- app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/sensors/Sensor.java
+++ app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/sensors/Sensor.java
@@ -2,12 +2,16 @@ package de.uni_augsburg.mobilesensingforfitnessandwellbeing.sensors;
 
 public abstract class Sensor {
 
-    public abstract void setWindowLength(int windowLenghtMillis);
+    private int windowLengthMillis;
 
     public abstract float getCurrentlyDesiredBpm();
 
     public abstract String getSensorName();
 
     public abstract double getRawSensorValue();
-    
+
+    public void setWindowLengthMillis(int windowLenghtMillis){
+        this.windowLengthMillis  = windowLenghtMillis;
+    }
+
 }
