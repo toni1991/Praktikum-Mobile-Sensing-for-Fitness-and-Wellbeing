9c15994e7e1b79f6b03856a290bd781a0031233a
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-02-27 15:12:02 +0100

    Added (not functional) settings and menubuttons.

--- app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/activity/JBpmActivity.java
+++ app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/activity/JBpmActivity.java
@@ -1,7 +1,11 @@
 package de.uni_augsburg.mobilesensingforfitnessandwellbeing.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import de.uni_augsburg.mobilesensingforfitnessandwellbeing.R;
 import de.uni_augsburg.mobilesensingforfitnessandwellbeing.view.InfoView;
@@ -19,6 +23,29 @@ public class JBpmActivity extends AppCompatActivity {
 
         findViews();
         init();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sensor_test:
+                Intent sensorTestIntent = new Intent(this, SensorTestActivity.class);
+                startActivity(sensorTestIntent);
+                return true;
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void findViews() {
