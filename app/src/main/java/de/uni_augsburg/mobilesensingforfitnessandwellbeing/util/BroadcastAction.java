8ed7e2ee3d4a1409f50b1bbabe95c683db562e84
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-02-28 16:58:47 +0100

    Added broadcast actions.

---
+++ app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/util/BroadcastAction.java
@@ -0,0 +1,34 @@
+package de.uni_augsburg.mobilesensingforfitnessandwellbeing.util;
+
+/**
+ * Created by toni on 28.02.18.
+ */
+
+public interface BroadcastAction {
+    interface PLAYBACK {
+        interface SKIP {
+            String ACTION = "de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.NEXT";
+            String EXTRA_SONG = "song";
+        }
+        interface PLAY {
+            String ACTION = "de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.PLAY";
+        }
+        interface PAUSE {
+            String ACTION = "de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.PAUSE";
+        }
+        interface PROGRESS {
+            String ACTION = "de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.PROGRESS";
+            String EXTRA_PROGRESS = "progress";
+        }
+    }
+
+    interface FILE {
+        interface REQUEST_NEXT_SONG {
+            String ACTION = "de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.REQUEST_NEXT_SONG";
+        }
+        interface NEXT_SONG {
+            String ACTION = "de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.NEXT_SONG";
+            String EXTRA_SONG = "song";
+        }
+    }
+}
