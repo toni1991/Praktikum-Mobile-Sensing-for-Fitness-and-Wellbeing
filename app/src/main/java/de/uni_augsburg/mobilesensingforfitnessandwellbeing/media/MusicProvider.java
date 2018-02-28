4710d1698344e7012914a7de97d7a1d32badaa2f
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-02-28 09:46:58 +0100

    Changed musicprovider interface.

--- app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/media/MusicProvider.java
+++ app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/media/MusicProvider.java
@@ -1,13 +1,32 @@
 package de.uni_augsburg.mobilesensingforfitnessandwellbeing.media;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+
 /**
  * Created by toni on 28.02.18.
  */
 
-public interface MusicProvider {
+public abstract class  MusicProvider {
 
-    BpmMappedSong getNextSong(float bpm);
+    private final Context context;
 
-    void dislike(BpmMappedSong bpmMappedSong);
+    protected MusicProvider(Context context)
+    {
+        this.context = context;
+    }
+
+    public abstract BpmMappedSong getNextSong(float bpm);
+
+    public abstract void dislike(BpmMappedSong bpmMappedSong);
+
+    final protected Uri getMediaDirectory()
+    {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this.context);
+        return Uri.parse(settings.getString("pref_media_directory", ""));
+    }
 
 }
