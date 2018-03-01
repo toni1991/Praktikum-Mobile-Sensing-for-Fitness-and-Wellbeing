bd02f2fcf07bf557ce8848d0b9a4eedef8fb95a5
Kevin Bichler <kevin.bichler@student.uni-augsburg.de>
2018-03-01 14:33:08 +0100

    changed SensorGraphView to receive values from Lukas

--- app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/view/SensorGraphView.java
+++ app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/view/SensorGraphView.java
@@ -1,7 +1,10 @@
 package de.uni_augsburg.mobilesensingforfitnessandwellbeing.view;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -16,6 +19,8 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+
+import de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.BroadcastAction;
 
 /**
  * Created by Kevin on 28.02.2018.
@@ -41,6 +46,7 @@ public class SensorGraphView
     private final Handler mHandler = new Handler();
     private LineGraphSeries<DataPoint> mSeries1;
     private LineGraphSeries<DataPoint> mSeries2;
+    private BroadcastReceiver receiver;
 
     private long startTime;
     private double graphLastXValue = 5d;
@@ -69,6 +75,8 @@ public class SensorGraphView
         this.pushNewDataPoint(sensor, newData);
     }
 
+
+
     private DataPoint getLatestPoint(String sensor)
     {
         ArrayList<DataPoint> tmp = this.sensorMap.get(sensor);
@@ -81,9 +89,38 @@ public class SensorGraphView
         return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
     }
 
+    private void registerBroadcastReceiver(Context context) {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BroadcastAction.VALUES.VALUEBROADCAST.ACTION);
+        context.registerReceiver(this.receiver, filter);
+    }
+
     public SensorGraphView(GraphView view)
     {
         this.graph = view;
+
+        this.receiver = new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, Intent intent)
+            {
+                switch (intent.getAction())
+                {
+                    case BroadcastAction.VALUES.VALUEBROADCAST.ACTION:
+                        if (intent.getStringExtra(BroadcastAction.VALUES.VALUEBROADCAST.EXTRA_VALUENAME).equals("bpm"))
+                        {
+                            String sensor = intent.getStringExtra(BroadcastAction.VALUES.VALUEBROADCAST.EXTRA_SENSORNAME);
+                            if (!sensorNames.contains(sensor))
+                            {
+                                registerSensor(sensor);
+                            }
+                            double value = intent.getDoubleExtra(BroadcastAction.VALUES.VALUEBROADCAST.EXTRA_VALUE,0.0d);
+                            pushNewData(sensor, value);
+                        }
+                        break;
+                }
+            }
+        };
     }
 
 //    public SensorGraphView(Context context) {
@@ -104,21 +141,21 @@ public class SensorGraphView
     public void init()
     {
         //this.graph = (GraphView) this;
-        Log.e("hm...", ""+this.graph.equals(null));
-
-        mSeries1 = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
-        });
-        mSeries2 = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 2),
-                new DataPoint(1, 4),
-                new DataPoint(2, 6)
-        });
-        Log.e("hm2...", ""+(this.graph == null));
-        this.graph.addSeries(mSeries1);
-        this.graph.addSeries(mSeries2);
+//        Log.e("hm...", ""+this.graph.equals(null));
+//
+//        mSeries1 = new LineGraphSeries<>(new DataPoint[] {
+//                new DataPoint(0, 1),
+//                new DataPoint(1, 5),
+//                new DataPoint(2, 3)
+//        });
+//        mSeries2 = new LineGraphSeries<>(new DataPoint[] {
+//                new DataPoint(0, 2),
+//                new DataPoint(1, 4),
+//                new DataPoint(2, 6)
+//        });
+//        Log.e("hm2...", ""+(this.graph == null));
+//        this.graph.addSeries(mSeries1);
+//        this.graph.addSeries(mSeries2);
         this.graph.getViewport().setXAxisBoundsManual(true);
         this.graph.getViewport().setMinX(0);
         this.graph.getViewport().setMaxX(40);
@@ -156,14 +193,17 @@ public class SensorGraphView
             @Override
             public void run()
             {
-//                seriesMap.keySet().forEach(s -> {
-//                    seriesMap.get(s).appendData(new DataPoint(getLatestPoint(s).getX(), getLatestPoint(s).getY()),
-//                            true, 1000);
-//                });
-                graphLastXValue += 1d;
-                mSeries1.appendData(new DataPoint(graphLastXValue, getRandom()), true, 40);
-                mSeries2.appendData(new DataPoint(graphLastXValue, getRandom()), true, 40);
-                mHandler.postDelayed(this, 500);
+                if (seriesMap.keySet().isEmpty())
+                {
+                    seriesMap.keySet().forEach(s -> {
+                        seriesMap.get(s).appendData(new DataPoint(getLatestPoint(s).getX(), getLatestPoint(s).getY()),
+                                true, 1000);
+                    });
+                }
+//                graphLastXValue += 1d;
+//                mSeries1.appendData(new DataPoint(graphLastXValue, getRandom()), true, 40);
+//                mSeries2.appendData(new DataPoint(graphLastXValue, getRandom()), true, 40);
+//                mHandler.postDelayed(this, 500);
             }
         };
     }
