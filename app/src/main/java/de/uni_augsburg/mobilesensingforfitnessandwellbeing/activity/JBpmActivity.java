6feda586702e70e9fa0dca276a13a2ee4ab3bcbb
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-03-01 17:58:43 +0100

    Changed permission handling.

--- app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/activity/JBpmActivity.java
+++ app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/activity/JBpmActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import com.jjoe64.graphview.GraphView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import de.uni_augsburg.mobilesensingforfitnessandwellbeing.R;
 import de.uni_augsburg.mobilesensingforfitnessandwellbeing.media.LocalMusicProvider;
@@ -35,6 +36,8 @@ public class JBpmActivity extends AppCompatActivity {
     private SensorGraphView sensorGraphView;
     private GraphView graphView;
     private MusicProvider musicProvider;
+    private int permissionRequestCode = 0;
+
     // Sensor Service
     private ArrayList<String> allPermissions;
 
@@ -45,12 +48,7 @@ public class JBpmActivity extends AppCompatActivity {
 
         this.allPermissions = new ArrayList<>();
 
-
-        requestPermissions(new String[]{
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE}
-        );
-
+        requestAllPermissions();
 
         findViews();
         startMusicService();
@@ -199,15 +197,24 @@ public class JBpmActivity extends AppCompatActivity {
     }
 
     private void initSensors() {
-        boolean request1 = requestPermissions(BTSensor.necessaryPermissions());
-        boolean request2 = requestPermissions(GpsSensor.necessaryPermissions());
-        if (request1 && request2) {
+        allPermissions.addAll(Arrays.asList(BTSensor.necessaryPermissions()));
+        allPermissions.addAll(Arrays.asList(GpsSensor.necessaryPermissions()));
+        if (checkPermissions()) {
             startSensorService();
         }
     }
 
-    private boolean requestPermissions(String[] permissions) {
-        boolean allPermissionsGranted = true;
+    private void requestAllPermissions() {
+
+        requestPermissions(new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.BLUETOOTH,
+                        Manifest.permission.BLUETOOTH_ADMIN},
+                permissionRequestCode++);
+
+        /*boolean allPermissionsGranted = true;
         for (String permission : permissions) {
             if (ActivityCompat.checkSelfPermission(this, permission) !=
                     PackageManager.PERMISSION_GRANTED) {
@@ -219,7 +226,7 @@ public class JBpmActivity extends AppCompatActivity {
 
             }
         }
-        return allPermissionsGranted;
+        return allPermissionsGranted;*/
     }
 
     @Override
