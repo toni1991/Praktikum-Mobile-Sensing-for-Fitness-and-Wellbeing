655d496732db9b56fc1ddab9ceae29297a95f666
Antonio Grieco <antonio.grieco@student.uni-augsburg.de>
2018-03-01 17:01:37 +0100

    Improved info view.

--- app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/view/InfoView.java
+++ app/src/main/java/de/uni_augsburg/mobilesensingforfitnessandwellbeing/view/InfoView.java
@@ -1,12 +1,16 @@
 package de.uni_augsburg.mobilesensingforfitnessandwellbeing.view;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 import de.uni_augsburg.mobilesensingforfitnessandwellbeing.R;
 import de.uni_augsburg.mobilesensingforfitnessandwellbeing.musicLibrary.MusicTrack;
+import de.uni_augsburg.mobilesensingforfitnessandwellbeing.util.BroadcastAction;
 
 /**
  * Created by toni on 27.02.18.
@@ -14,33 +18,71 @@ import de.uni_augsburg.mobilesensingforfitnessandwellbeing.musicLibrary.MusicTra
 
 public class InfoView extends ConstraintLayout {
 
+    private TextView bpmTextViewCurrentSong;
+    private TextView bpmTextViewEstimated;
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+            switch (action) {
+                case BroadcastAction.FILE.NEXT_SONG.ACTION:
+                    MusicTrack musicTrack = intent.getParcelableExtra(BroadcastAction.FILE.NEXT_SONG.EXTRA_SONG);
+                    setCurrentSongBpm(musicTrack != null ? musicTrack.getBpm() : 0.0f);
+                    break;
+                case BroadcastAction.VALUES.BPMESTIMATION.ACTION:
+                    float bpm = intent.getFloatExtra(BroadcastAction.VALUES.BPMESTIMATION.EXTRA_VALUEBPM, 0.0f);
+                    setEstimatedBpm(bpm);
+                    break;
+
+            }
+        }
+    };
+
     public InfoView(Context context) {
         super(context);
-        ConstraintLayout.inflate(context, R.layout.view_info, this);
+        init(context);
     }
 
     public InfoView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        ConstraintLayout.inflate(context, R.layout.view_info, this);
+        init(context);
     }
 
     public InfoView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
         ConstraintLayout.inflate(context, R.layout.view_info, this);
+        findViews();
     }
 
-    public void setBpmTextView(float bpm)
-    {
-        String bpmAsString = String.format("%.2f", bpm) ;
-        setBpmTextView(bpmAsString);
+    private void findViews() {
+        bpmTextViewCurrentSong = findViewById(R.id.bpmTextViewCurrentSong);
+        bpmTextViewEstimated = findViewById(R.id.bpmTextViewEstimated);
     }
 
-    public void setBpmTextView(String bpmAsString)
-    {
-        ((TextView)findViewById(R.id.bpmTextView)).setText(bpmAsString);
+    private void setCurrentSongBpm(float bpm) {
+        bpmTextViewCurrentSong.setText(String.format("%.2f", bpm));
     }
 
-    public void setCurrentSong(MusicTrack bpmMappedSong) {
-        setBpmTextView(bpmMappedSong.getBpm());
+    private void setEstimatedBpm(float bpm) {
+        bpmTextViewEstimated.setText(String.format("%.2f", bpm));
     }
+
+    public BroadcastReceiver getBroadcastReceiver() {
+        return this.broadcastReceiver;
+    }
+
+    public IntentFilter getIntentFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BroadcastAction.FILE.NEXT_SONG.ACTION);
+        filter.addAction(BroadcastAction.VALUES.BPMESTIMATION.ACTION);
+        return filter;
+    }
+
+
 }
